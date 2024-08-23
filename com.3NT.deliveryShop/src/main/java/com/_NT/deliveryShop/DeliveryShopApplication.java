cf2bffb5aaf5005603c506f612caedb25b15c501
sanghun <tkdgns0236@gmail.com>
2024-08-23 12:49:56 +0900

    init commit

---
+++ com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/DeliveryShopApplication.java
@@ -0,0 +1,13 @@
+package com._NT.deliveryShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryShopApplication.class, args);
+	}
+
+}
---
+++ com.3NT.deliveryShop/src/test/java/com/_NT/deliveryShop/DeliveryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com._NT.deliveryShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
