2d84723921890e9e4634dedbcafd77192f427fe1
sanghun <tkdgns0236@gmail.com>
2024-08-27 10:49:21 +0900

    Feat: DTO 클래스 이름 재정의 및 메소드 추가 #27

--- com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/controller/UserController.java
+++ com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/controller/UserController.java
@@ -6,7 +6,6 @@ import com._NT.deliveryShop.security.UserDetailsImpl;
 import com._NT.deliveryShop.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -25,15 +24,15 @@ public class UserController {
 
     @PostMapping
     @ResponseBody
-    public UserDto.Response createUser(@RequestBody UserDto.Create userDto) {
+    public UserDto.Result createUser(@RequestBody UserDto.Create userDto) {
 
         return userService.createUser(userDto);
     }
 
     @GetMapping("/{userId}")
     @ResponseBody
-    public UserDto.Response getUser(@PathVariable(value = "userId") Long userId,
-                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public UserDto.Result getUser(@PathVariable(value = "userId") Long userId,
+                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         return userService.getUser(userId, userDetails.getUser());
     }
@@ -44,22 +43,22 @@ public class UserController {
     public Collection<UserDto.GetAllUsersResponse> getAllUsers(
             @RequestParam("page") int page,
             @RequestParam("size") int size,
-            @RequestParam("sort") String sort,
-            @AuthenticationPrincipal UserDetailsImpl userDetails){
-        return userService.getAllUsers(userDetails.getUser(), page - 1, size, sort);
+            @RequestParam("sort") String sort){
+        return userService.getAllUsers(page - 1, size, sort);
     }
 
     @PatchMapping("/{userId}")
     @ResponseBody
-    public UserDto.ModifyUserResponse modifyUser(@PathVariable(value = "userId") Long userId,
-                                       @RequestBody UserDto.Modify userDto,
-                                       @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public UserDto.ModifyUserResult modifyUser(@PathVariable(value = "userId") Long userId,
+                                               @RequestBody UserDto.Modify userDto,
+                                               @AuthenticationPrincipal UserDetailsImpl userDetails){
         return userService.modifyUser(userId, userDto, userDetails.getUser());
     }
 
 
     @DeleteMapping("/{userId}")
-    public Long deleteUser(@PathVariable(value = "userId") Long userId,
+    @ResponseBody
+    public UserDto.DeleteUserResult deleteUser(@PathVariable(value = "userId") Long userId,
                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         return userService.deleteUser(userId, userDetails.getUser());
