ae26d085dd4bf1db45aedf7b82826b181e7a0138
sanghun <tkdgns0236@gmail.com>
2024-08-27 10:49:49 +0900

    Refactor: @Repository 어노테이션 작성 #27

--- com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/repository/UserRepository.java
+++ com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com._NT.deliveryShop.repository;
 
 import com._NT.deliveryShop.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
