4b35b5a726e406794f70d51fcf2eb6455af53c46
tellang <tellang@daum.net>
2024-08-27 10:17:12 +0900

    feat: Timestamped에 Setter 추가
    isDeleted, deletedBy 값 입력을 위한 Setter 추가
    
    Closes #7

--- com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/domain/entity/Timestamped.java
+++ com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/domain/entity/Timestamped.java
@@ -1,15 +1,20 @@
 package com._NT.deliveryShop.domain.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
+@Setter
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
