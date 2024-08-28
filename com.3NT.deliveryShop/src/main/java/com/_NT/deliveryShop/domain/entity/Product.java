51dd33f3576bda66bdeb453b8900b2faea7c0166
tellang <tellang@daum.net>
2024-08-28 14:13:31 +0900

    fix: ProductBuilder에서 productId 필드를 제거
    외부 주입이 아닌 자동 생성으로 만들어지는 productId를 builder 패턴으로 생성시 주입이 불가능 하도록 builder에서 사용할 생성자를 지정

--- com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/domain/entity/Product.java
+++ com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/domain/entity/Product.java
@@ -1,16 +1,27 @@
 package com._NT.deliveryShop.domain.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
-import java.util.UUID;
 
 @Entity
 @Table(name = "p_product")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product extends Timestamped {
     @Id
     @GeneratedValue(generator = "UUID")
@@ -34,4 +45,14 @@ public class Product extends Timestamped {
 
     @Column(name = "image_url", nullable = false)
     private String imageURL;
+
+    @Builder
+    public Product(Restaurant restaurant, String name, String description, Integer price,
+        String imageURL) {
+        this.restaurant = restaurant;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageURL = imageURL;
+    }
 }
