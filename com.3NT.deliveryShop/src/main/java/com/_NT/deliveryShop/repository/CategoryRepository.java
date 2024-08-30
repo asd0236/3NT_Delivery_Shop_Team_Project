f970fef3ff61a42ca39757dec91f9e1c0eb451a3
tellang <tellang@daum.net>
2024-08-30 11:20:52 +0900

    feat: Category 의 soft delete 구현
    - 필드값을 변경하여 삭제를 구현한 softDeleteCategory 구현

--- com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/repository/CategoryRepository.java
+++ com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/repository/CategoryRepository.java
@@ -1,11 +1,20 @@
 package com._NT.deliveryShop.repository;
 
 import com._NT.deliveryShop.domain.entity.Category;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 
+    @Modifying
+    @Query(
+        "UPDATE Category c SET c.isDeleted = true, c.deletedAt = :deletedAt, c.deletedBy = :deletedBy "
+            +
+            "WHERE c.categoryId = :categoryId")
+    void softDeleteCategory(UUID categoryId, LocalDateTime deletedAt, Long deletedBy);
 }
