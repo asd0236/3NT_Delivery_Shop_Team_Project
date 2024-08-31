3d70a63c3fb4bd44f1d5b42199dab7c72d7ed85f
tellang <tellang@daum.net>
2024-08-31 21:36:20 +0900

    feat: AmazonS3FileDto 구현
    AmazonS3FileService 와 각 업로드 파일이 필요한 엔티티의 서비스간의 의존성은 담당할 Dto

---
+++ com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/domain/dto/AmazonS3FileDto.java
@@ -0,0 +1,61 @@
+package com._NT.deliveryShop.domain.dto;
+
+import com._NT.deliveryShop.domain.entity.ProductImg;
+import com._NT.deliveryShop.domain.entity.UploadFile;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Data;
+import lombok.With;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AmazonS3FileDto {
+
+    @With
+    @Data
+    @Builder
+    class Put {
+
+        private UUID likedUUID;
+        private MultipartFile multipartFile;
+
+        public UploadFile asEntityWith(Result result) {
+            return UploadFile.builder()
+                .likedUUID(likedUUID)
+                .originalFileName(result.getOriginalFileName())
+                .uploadFilePath(result.getUploadFilePath())
+                .uploadFileName(result.getUploadFileName())
+                .uploadFileUrl(result.getUploadFileUrl())
+                .build();
+        }
+    }
+
+
+    @With
+    @Data
+    @Builder
+    class Result {
+
+        private String originalFileName;
+        private String uploadFileName;
+        private String uploadFilePath;
+        private String uploadFileUrl;
+
+        public static Result of(ProductImg productImg) {
+            return Result.builder()
+                .originalFileName(productImg.getOriginalFileName())
+                .uploadFileName(productImg.getUploadFileName())
+                .uploadFilePath(productImg.getUploadFilePath())
+                .uploadFileUrl(productImg.getUploadFileUrl())
+                .build();
+        }
+
+        @Data
+        @Builder
+        public static class Deleted {
+
+            private String result;
+            private String originalFileName;
+            private String deletedUploadFileUrl;
+        }
+    }
+}
