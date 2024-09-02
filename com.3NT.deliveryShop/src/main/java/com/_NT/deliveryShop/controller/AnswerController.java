ca866fe2b6ad2febbe0a738c78567295be0d60c4
tellang <tellang@daum.net>
2024-09-02 14:39:35 +0900

    feat: 신고 게시글 답변 swagger 적용

--- com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/controller/AnswerController.java
+++ com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/controller/AnswerController.java
@@ -7,6 +7,10 @@ import static com._NT.deliveryShop.domain.entity.UserRoleEnum.PreAuthorizeRole.A
 
 import com._NT.deliveryShop.service.AnswerService;
 import com._NT.deliveryShop.service.authorizer.AnswerAuthorizer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "신고 게시글 답변", description = "신고 게시글 답변 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
@@ -35,6 +40,8 @@ public class AnswerController {
     @PreAuthorize("hasRole(" + ADMIN + ")")
     @PostMapping("/answers")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "신고 게시글 답변 등록", description = "신고 게시글 답변을 등록합니다.")
+    @ApiResponse(responseCode = "201", description = "신고 게시글 답변 등록 성공")
     public Result postAnswer(@RequestBody Create dto, Authentication authentication) {
 
         answerAuthorizer.requireByOneself(authentication, dto.getOwnerId());
@@ -42,7 +49,12 @@ public class AnswerController {
     }
 
     @GetMapping("/reports/{reportId}/answers")
-    public Result getAnswer(@PathVariable UUID reportId, Authentication authentication) {
+    @Operation(summary = "신고 게시글을 기준으로 신고 게시글 답변 단건 조회", description = "신고 게시글 답변을 단건 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "신고 게시글 답변 조회 성공")
+
+    public Result getAnswer(
+        @Schema(description = "신고 게시글 식별자", example = "UUID")
+        @PathVariable UUID reportId, Authentication authentication) {
 
         answerAuthorizer.requireReportOwner(authentication, reportId);
         return service.readAnswerByReportId(reportId);
@@ -50,6 +62,8 @@ public class AnswerController {
 
     @PreAuthorize("hasRole(" + ADMIN + ")")
     @GetMapping("/answers")
+    @Operation(summary = "신고 게시글 답변 전체 조회", description = "신고 게시글 답변을 전체 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "신고 게시글 답변 전체 조회 성공")
     public List<Result> getAllAnswer(Pageable pageable) {
 
         return service.readAllAnswer(pageable);
@@ -57,7 +71,11 @@ public class AnswerController {
 
     @PreAuthorize("hasRole(" + ADMIN + ")")
     @PutMapping("/answers/{id}")
-    public Result putAnswer(@PathVariable UUID id,
+    @Operation(summary = "신고 게시글 답변 수정", description = "신고 게시글 답변을 수정합니다.")
+    @ApiResponse(responseCode = "200", description = "신고 게시글 답변 수정 성공")
+    public Result putAnswer(
+        @Schema(description = "신고 게시글 답변 식별자", example = "UUID")
+        @PathVariable UUID id,
         @RequestBody Put dto, Authentication authentication) {
 
         answerAuthorizer.requireByOneself(authentication, dto.getUpdater());
@@ -66,7 +84,11 @@ public class AnswerController {
 
     @PreAuthorize("hasRole(" + ADMIN + ")")
     @DeleteMapping("/answers/{id}")
-    public Result.Deleted deleteAnswer(@PathVariable UUID id, Authentication authentication) {
+    @Operation(summary = "신고 게시글 답변 삭제", description = "신고 게시글 답변을 소프트 삭제합니다.")
+    @ApiResponse(responseCode = "200", description = "신고 게시글 답변 삭제 성공")
+    public Result.Deleted deleteAnswer(
+        @Schema(description = "신고 게시글 답변 식별자", example = "UUID")
+        @PathVariable UUID id, Authentication authentication) {
 
         return service.deleteAnswer(id, authentication);
     }
