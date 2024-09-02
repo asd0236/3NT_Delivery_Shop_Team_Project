34aafb876f623c827c350c232834a16ec3e85e70
jungbin97 <ljb5054@gmail.com>
2024-09-02 15:13:19 +0900

    feat: 주문(Order) 공통 응답처리 및 예외 처리

--- com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/controller/OrderController.java
+++ com.3NT.deliveryShop/src/main/java/com/_NT/deliveryShop/controller/OrderController.java
@@ -1,13 +1,7 @@
 package com._NT.deliveryShop.controller;
 
-import static com._NT.deliveryShop.domain.dto.OrderDto.CreateOrderRequest;
-import static com._NT.deliveryShop.domain.dto.OrderDto.CreateOrderResponse;
-import static com._NT.deliveryShop.domain.dto.OrderDto.DeleteResponse;
-import static com._NT.deliveryShop.domain.dto.OrderDto.ModifyRequest;
-import static com._NT.deliveryShop.domain.dto.OrderDto.OrderResponse;
-import static com._NT.deliveryShop.domain.entity.UserRoleEnum.PreAuthorizeRole.ADMIN;
-import static com._NT.deliveryShop.domain.entity.UserRoleEnum.PreAuthorizeRole.USER;
-
+import com._NT.deliveryShop.common.codes.SuccessCode;
+import com._NT.deliveryShop.common.response.ResultResponse;
 import com._NT.deliveryShop.security.UserDetailsImpl;
 import com._NT.deliveryShop.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,22 +9,16 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+import static com._NT.deliveryShop.domain.dto.OrderDto.*;
+import static com._NT.deliveryShop.domain.entity.UserRoleEnum.PreAuthorizeRole.*;
 
 @Tag(name = "주문", description = "주문 등록, 조회, 수정, 삭제 API")
 @RestController
@@ -44,39 +32,56 @@ public class OrderController {
     @PreAuthorize("hasAnyRole(" + ADMIN +  "," + USER + ")")
     @Operation(summary = "주문 등록", description = "주문을 등록합니다.")
     @ApiResponse(responseCode = "201", description = "주문 등록 성공")
-    @ResponseStatus(HttpStatus.CREATED)
-    public CreateOrderResponse createOrder(@RequestBody CreateOrderRequest orderRequestDto,
-                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return orderService.createOrder(orderRequestDto, userDetails.getUser());
+    public ResultResponse<CreateOrderResponse> createOrder(@RequestBody CreateOrderRequest orderRequestDto,
+                                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CreateOrderResponse order = orderService.createOrder(orderRequestDto, userDetails.getUser());
+
+        return ResultResponse.<CreateOrderResponse>builder()
+                .result(order)
+                .resultCode(SuccessCode.INSERT_SUCCESS.getStatus())
+                .resultMessage(SuccessCode.INSERT_SUCCESS.getMessage())
+                .build();
     }
 
     // 주문 단건 조회
     @GetMapping("/{orderId}")
     @Operation(summary = "주문 단건 조회", description = "주문을 단건 조회합니다.")
     @ApiResponse(responseCode = "200", description = "주문 조회 성공")
-    public OrderResponse getOrder(
+    public ResultResponse<OrderResponse> getOrder(
             @Parameter(description = "주문 식별자", example = "UUID", required = true)
             @PathVariable UUID orderId,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return orderService.getOrder(orderId, userDetails.getUser());
+        OrderResponse order = orderService.getOrder(orderId, userDetails.getUser());
+
+        return ResultResponse.<OrderResponse>builder()
+                .result(order)
+                .resultCode(SuccessCode.SELECT_SUCCESS.getStatus())
+                .resultMessage(SuccessCode.SELECT_SUCCESS.getMessage())
+                .build();
     }
 
     // 주문 삭제
     @DeleteMapping("/{orderId}")
     @Operation(summary = "주문 삭제", description = "주문을 소프트 삭제합니다.")
     @ApiResponse(responseCode = "200", description = "주문 삭제 성공")
-    public DeleteResponse deleteOrder(
+    public ResultResponse<DeleteResponse> deleteOrder(
             @Parameter(description = "주문 식별자", example = "UUID", required = true)
             @PathVariable UUID orderId,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return orderService.deleteOrder(orderId, userDetails.getUser());
+        DeleteResponse deleteResponse = orderService.deleteOrder(orderId, userDetails.getUser());
+
+        return ResultResponse.<DeleteResponse>builder()
+                .result(deleteResponse)
+                .resultCode(SuccessCode.DELETE_SUCCESS.getStatus())
+                .resultMessage(SuccessCode.DELETE_SUCCESS.getMessage())
+                .build();
     }
 
     // 주문 전체 조회
     @GetMapping
     @Operation(summary = "주문 전체 조회", description = "주문을 전체 조회합니다.")
     @ApiResponse(responseCode = "200", description = "주문 전체 조회 성공")
-    public Page<OrderResponse> getOrders(
+    public ResultResponse<Page<OrderResponse>> getOrders(
             @Schema(description = "페이지 번호(0부터 N까지)", defaultValue = "0")
             @RequestParam("page") int page,
             @Schema(description = "페이지에 출력할 개수를 입력합니다.", defaultValue = "10")
@@ -86,19 +91,30 @@ public class OrderController {
             @Schema(description = "정렬 방식을 입력합니다.", defaultValue = "true")
             @RequestParam("isAsc") boolean isAsc,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return orderService.getAllOrders(page, size, sortBy, isAsc, userDetails.getUser());
+        Page<OrderResponse> allOrders = orderService.getAllOrders(page, size, sortBy, isAsc, userDetails.getUser());
+
+        return ResultResponse.<Page<OrderResponse>>builder()
+                .result(allOrders)
+                .resultCode(SuccessCode.SELECT_SUCCESS.getStatus())
+                .resultMessage(SuccessCode.SELECT_SUCCESS.getMessage())
+                .build();
     }
 
     // 주문 수정
     @PatchMapping("/{orderId}")
     @Operation(summary = "주문 수정", description = "주문을 수정합니다.")
     @ApiResponse(responseCode = "200", description = "주문 수정 성공")
-    public OrderResponse modifyOrder(
+    public ResultResponse<OrderResponse> modifyOrder(
             @Parameter(description = "주문 식별자", example = "UUID", required = true)
             @PathVariable UUID orderId,
             @RequestBody ModifyRequest orderModifyRequestDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return orderService.modifyOrder(orderId, orderModifyRequestDto, userDetails.getUser());
+        OrderResponse orderResponse = orderService.modifyOrder(orderId, orderModifyRequestDto, userDetails.getUser());
+        return ResultResponse.<OrderResponse>builder()
+                .result(orderResponse)
+                .resultCode(SuccessCode.UPDATE_SUCCESS.getStatus())
+                .resultMessage(SuccessCode.UPDATE_SUCCESS.getMessage())
+                .build();
     }
 
 }
